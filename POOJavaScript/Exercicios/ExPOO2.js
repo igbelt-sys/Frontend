77ee3d78fe81bf9a1153d913494abc42f6ed54d2
igor beltramo <beltramoigor1@gmail.com>
2026-04-09 16:06:57 -0300

    corrigido

--- POOJavaScript/Exercicios/ExPOO2.js
+++ POOJavaScript/Exercicios/ExPOO2.js
@@ -9,13 +9,28 @@ class Veiculo {
     this.modelo = modelo;
     this.ano = ano;
   }
-
   exibirInfo() {
     console.log(`Marca: ${this.marca}`);
     console.log(`Modelo: ${this.modelo}`);
     console.log(`Ano: ${this.ano}`);
   }
 }
- let c1 = new Veiculo ("Hyundai", "civic g11", 2011)
 
- c1.exibirInfo()
+//criar uma subclassse de veiculos
+class Carro extends Veiculo {
+    portas;
+
+    constructor(marca, modelo, ano, portas) {
+        super(marca, modelo, ano);
+        this.portas = portas;
+    }
+
+    exibirInfo() {
+        super.exibirInfo();
+        console.log(`Portas: ${this.portas}`);
+    }
+}
+
+let c1 = new Carro("Hyundai", "Civic G11", 2011, 4);
+
+c1.exibirInfo();
